cc19d45ce509d00fe6b52361c7f495db3e368a08
Josh Graff <joshgraff@ocu3.(none)>
2016-09-09 15:32:16 -0400

    connects to Joshuagraff.com and prints the page

--- client.c
+++ client.c
@@ -10,60 +10,102 @@ Programming examples found from http://beej.us/guide/bgnet/output/print/bgnet_US
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
 
-
-	int status, sfd;
+	int status, sfd, request_len;
+	ssize_t nread;
 	char* hostname;
 	char* portnum;
-	struct addrinfo *connection_addr_info;
-	struct addrinfo *hints;
+	char* received_buffer;
+	int received_buffer_size;
+	struct addrinfo *addr_info, *rp, *hints;
 
 int main(int argc, char *argv[])
 {
-
+	request_len =85;
+	received_buffer_size = 2000;
+	received_buffer = realloc(received_buffer,sizeof(char)*received_buffer_size);
 	hostname = realloc(hostname,(sizeof(char)*100));
 	portnum = realloc(portnum,(sizeof(char)*20));
 	hints = realloc(hints,sizeof(hints));
 	portnum = "80";
-	
-	//Beej.us textbook
-	hints->ai_family = AF_UNSPEC; // don't care IPv4 or IPv6
-	hints->ai_socktype = SOCK_STREAM; // TCP stream sockets
-	hints->ai_flags = AI_PASSIVE; // fill in my IP for me
+	char ipstr[INET6_ADDRSTRLEN];
+	//Man Pages
+	hints->ai_family = AF_INET;    		//IPV4
+	hints->ai_socktype = SOCK_STREAM; 	//TCP
+	hints->ai_flags = 0;
+	hints->ai_protocol = 0;
+
 
 	if (argc < 2)
 	{
 		printf("Not enough arguments\n");	
-		printf("usage: ./client \'hostname\'");
+		printf("usage: ./client \'hostname\'\n");
 		return 1;	
 	}else{
 		hostname = argv[1];
 	}
 
-	if(getaddrinfo(hostname,portnum,hints,&connection_addr_info))
+	if(getaddrinfo(hostname,portnum,hints,&addr_info))
 	{
 		printf("error retrieving info\n");
 		return 1;
 	}
 
-	//man pages
-	for (connection_addr_info; connection_addr_info != NULL; connection_addr_info = connection_addr_info->ai_next) 
-	{
-		sfd = socket(connection_addr_info->ai_family, connection_addr_info->ai_socktype,
-			   connection_addr_info->ai_protocol);
-		if (sfd == -1)
-			continue;
-
-		if (bind(sfd, connection_addr_info->ai_addr, connection_addr_info->ai_addrlen) == 0)
-		{
-			printf("success\n");
-		   	break;                  /* Success */
+/*
+//prints the IPAddr; from the text that kinnicky linked us.
+	for(rp = addr_info;rp != NULL; rp = rp->ai_next) {
+		void *addr;
+		char *ipver;
+		// get the rpointer to the address itself,
+		// different fields in Irpv4 and Irpv6:
+		if (rp->ai_family == AF_INET) { // Irpv4
+			struct sockaddr_in *ipv4 = (struct sockaddr_in *)rp->ai_addr;
+			addr = &(ipv4->sin_addr);
+			ipver = "Ipv4";
+		} else { // Irpv6
+			struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *)rp->ai_addr;
+			addr = &(ipv6->sin6_addr);
+			ipver = "Ipv6";
 		}
+		// convert the Irp to a string and print it:
+		inet_ntop(rp->ai_family, addr, ipstr, sizeof ipstr);
+		printf(" %s: %s\n", ipver, ipstr);
+	}
+*/
 
-		close(sfd);
-   	}
+	sfd = socket(addr_info->ai_family, addr_info->ai_socktype,addr_info->ai_protocol);
+
+	if (connect(sfd, addr_info->ai_addr, addr_info->ai_addrlen) != -1) //we're connected
+		printf("success\n");
+
+
+	if(addr_info == NULL)
+	{
+		printf("failed to connect\n");
+		return 1;
+	}
+	freeaddrinfo(addr_info);
 
 
 
-	freeaddrinfo(connection_addr_info);
+	//request data
+	if((status = send(sfd, "GET /Obama.html HTTP/1.0\r\nHost: Joshuagraff.com\r\nUser-Agent: Mobile/7B405\r\n\r\n", request_len,0)) != request_len) 
+	{
+		fprintf(stderr, "partial/failed write\n");
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", status);
+
+	//receive data
+	nread = recv(sfd, received_buffer, received_buffer_size,0);
+	if (nread == -1) 
+	{
+		perror("read");
+		exit(EXIT_FAILURE);
+	}
+	close(sfd);
+	printf("Received %ld bytes: %s\n", (long) nread, received_buffer);
+
 }
